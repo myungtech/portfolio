9135490f09f5c57df7f167ab830fd64d10ebfb54
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-01-21 05:06:31 +0900

    JS : Home contact button

--- main.js
+++ main.js
@@ -21,18 +21,27 @@ document.addEventListener('scroll',()=>{
 });
 
 // Handle scrolling when tapping on the navbar menu
+// 클릭했을때 원하는 아이디를 알아야한다.
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click',(event)=>{
+    
     const target = event.target;
     const link = target.dataset.link;
 
     if(link == null){
         return;
     }
-    
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView( {behavior: "smooth"});
-
+    // console.log(event.target.dataset.link);
+    scrollIntoView(link);
 });
 
+//Handel click on "contact me" buttom on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click',(event)=>{
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView( {behavior: "smooth"});
+}
